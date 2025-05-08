53c17721e510c2ca1f17f4e6d75f6906a588cfb0
User2 <vkagarmanova@inbox.ru>
2025-05-08 22:37:31 +0500

    упаковано в docker

--- medical-service/target/generated-sources/annotations/medical_service/mapper/VaccinationMapperImpl.java
+++ medical-service/target/generated-sources/annotations/medical_service/mapper/VaccinationMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-08T10:25:54+0500",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (BellSoft)"
+    date = "2025-05-08T22:31:24+0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
 )
 @Component
 public class VaccinationMapperImpl implements VaccinationMapper {
